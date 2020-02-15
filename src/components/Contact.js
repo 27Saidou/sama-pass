6bdcd17ddcfa5c1c424128d8379a6c2e1eddc76d
Saidou Diallo <sinthio2@gmail.com>
2020-02-15 20:03:48 +0000

    add  Router,Link,NavLink and withRouter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Contact=()=>{
+const Contact=(props)=>{
+    // setTimeout(()=>{
+    //     props.history.push('/about')
+    // },2000);
     return(
         <div className="container">
             <h2 className="center">Contact</h2>
-                   <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.Le Lorem Ipsum est le faux texte standard de l 'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte</p>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.Le Lorem Ipsum est le faux texte standard de l 'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte</p>
         </div>
     )
 }
