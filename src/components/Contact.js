d97ecf406664d0ac245d9cb71912290a6f0a6e1a
Saidou Diallo <sinthio2@gmail.com>
2020-02-15 19:37:20 +0000

    initial commit add navbar and router

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact=()=>{
+    return(
+        <div className="container">
+            <h2 className="center">Contact</h2>
+                   <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.Le Lorem Ipsum est le faux texte standard de l 'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte</p>
+        </div>
+    )
+}
+
+export default Contact
