325c837e285baa46cba0f5ba7be4d409fff2c3e5
Saidou Diallo <sinthio2@gmail.com>
2020-02-17 02:50:28 +0000

    add Post and i installed axios and import image pokeball

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import { BrowserRouter,Route} from 'react-router-dom'
  import Home from './components/Home'
- import About from './components/About';
+ import About from './components/About'
  import Contact from './components/Contact'
+ import Post from './components/Post'
 
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route  exact path='/'component={Home}/>
+        <Route exact path='/'component={Home}/>
         <Route path='/about'component={About}/>
         <Route path='/contact'component={Contact}/>
+        <Route path='/:post_id' component={Post}/> 
       </div>
     </BrowserRouter>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
-const Home=()=>{
-    return(
-    <div className="container">
-        <h4 className="center">Home</h4>
-        <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.Le Lorem Ipsum est le faux texte standard de l 'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte</p>
-    </div>
-    )
+
+
+class Home extends Component {
+    state={
+        posts:[]
+    }
+    componentDidMount(){
+axios.get('https://jsonplaceholder.typicode.com/posts')
+.then(res=>{
+    console.log(res)
+    this.setState({
+   posts:res.data.slice(0,10)
+    })
+    
+})
+    }
+    render(){
+        const {posts}=this.state;
+        const postList=posts.length ?(
+            posts.map(post=>{
+                return(
+                   <div className="post card" key={post.id}>
+                   <img src={Pokeball} alt=" hey koto"/>
+                    <div className="card-content">
+                    <Link to={'/'+ post.id}>
+                        <span className="card-title red-text">{post.title}</span>
+                        </Link>
+                        <p>{post.body}</p>
+                    </div>
+                   </div>
+                )
+            })
+        ):(
+            <div className="center">Pas d'article
+            </div>
+        )
+        return ( 
+          <div className = "container home">
+            <h4 className = "center"> Home </h4> 
+            {postList}
+            </div>
+        )
+    }
+    
 }
 export default Home
